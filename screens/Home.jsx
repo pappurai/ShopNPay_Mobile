bb1155e680fa1ccf76b69e321619f7fb1b107ec3
Rahul <rkc.98@outlook.com>
2021-12-12 11:29:29 +0530

    added demo home page

---
+++ screens/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+    return (
+        <View styles={{ flex: 1 }}>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
